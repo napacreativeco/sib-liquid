95b15f91c7aa4bf21a70e71ec7ea804ae6e9d395
throne <contact@throne.work>
2023-09-20 11:11:07 -0700

    sign up testing

--- src/js/throne.js
+++ src/js/throne.js
@@ -229,7 +229,7 @@
 
       $(document).on('click', function(e) {
         e.preventDefault();
-        if (e.target.matches('.modal-wrapper') || e.target.matches('input') || e.target.matches('select') || e.target.matches('button')) {
+        if (e.target.matches('.modal-wrapper') || e.target.matches('input') || e.target.matches('select') || e.target.matches('form')) {
           
         } else {
           $('.sign-up-modal').css('display', 'none');
